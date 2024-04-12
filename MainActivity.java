2a269ea5d91b0947e634a44a9d6976d80c296922
Parvez20000 <parvezkhan70788@gmail.com>
2024-04-12 20:59:45 +0530

    parvez bhai

--- MainActivity.java
+++ MainActivity.java
@@ -9,8 +9,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
-
-public class MainActivity extends AppCompatActivity {
+djerughfds
+sdfkjbdfsjhflsfjg
+sdfkhbsdhf
+sdfkhbskhf
+public class MainActivityparvez extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private View.OnClickListener clickListener;
